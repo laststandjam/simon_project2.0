cb08a290d5c21ef53194bcbde5258ee618294b07
ihavehands <zaneshook@gmail.com>
2019-12-17 18:49:27 -0800

    const and state

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//const
+const colors = ["#red","#yellow", "#green", "#blue"]
+
+//cached
+
+//state
+let currentPattern,
+    playerPattern,
+    rounds
+
+
+//functions
+function newGame() {
+    currentPattern = []
+    playerPattern = []
+    rounds = 0
+}
