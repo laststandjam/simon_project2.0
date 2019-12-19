c3978a63aa75f67564a833d0eb910cd5e84e25ac
ihavehands <zaneshook@gmail.com>
2019-12-18 17:31:53 -0800

    working need to still end loop

--- js/main.js
+++ js/main.js
@@ -1,100 +1,106 @@
-
-
 //const
 
-
-const colors = ["red","yellow", "green", "blue"]
+const colors = ["red", "yellow", "green", "blue"];
 
 //cached
-var red = document.getElementById("red")
-var yellow = document.getElementById("yellow")
-var green = document.getElementById("green")
-var blue = document.getElementById("blue")
-var rounds = document.getElementById("rounds")
-var startButton=document.getElementById("gameStart")
-var headerText = document.getElementById("gameMessage")
+var red = document.getElementById("red");
+var yellow = document.getElementById("yellow");
+var green = document.getElementById("green");
+var blue = document.getElementById("blue");
+var rounds = document.getElementById("rounds");
+var startButton = document.getElementById("gameStart");
+var headerText = document.getElementById("gameMessage");
 //state
-let currentPattern,
-    playerPattern,
-    roundCounter;
-
+let currentPattern, playerPattern, roundCounter, lost;
 
 //listernes
-red.addEventListener("click", track)
-yellow.addEventListener("click", track)
-green.addEventListener("click", track)
-blue.addEventListener("click", track)
-startButton.addEventListener("click",startGame )
+red.addEventListener("click", track);
+yellow.addEventListener("click", track);
+green.addEventListener("click", track);
+blue.addEventListener("click", track);
+startButton.addEventListener("click", startGame);
 
 //functions
-   
-init()
+
+init();
 function init() {
-    currentPattern = []
-    playerPattern = []
-    roundCounter = 0
+  currentPattern = [];
+  playerPattern = [];
+  roundCounter = 0;
 }
 function newGame() {
-    currentPattern = []
-    playerPattern = []
-    roundCounter = 1
+  currentPattern = [];
+  playerPattern = [];
+  roundCounter = 1;
 }
-function randomizer(){
-    let i = Math.floor(Math.random() * colors.length)
-    console.log(colors[i])
-    currentPattern.push(colors[i])
+function randomizer() {
+  let i = Math.floor(Math.random() * colors.length);
+  console.log(colors[i]);
+  currentPattern.push(colors[i]);
 }
- function blinker() {
-    let i = 0;
-    let interval = setInterval(function() {
-      $("#" + currentPattern[i]).fadeTo("slow", 0).fadeTo("slow", 1);
-      i++;
-      if (i >= currentPattern.length) {
-        clearInterval(interval);
-      }
-    }, 1500);
-  };
-
-function track(){
-    playerPattern.push(event.target.id)
-    judger()
-    if(playerPattern.length === currentPattern.length){
-    newRound()
+function blinker() {
+  let i = 0;
+  let interval = setInterval(function() {
+    $("#" + currentPattern[i])
+      .fadeTo("slow", 0)
+      .fadeTo("slow", 1);
+    i++;
+    if (i >= currentPattern.length) {
+      clearInterval(interval);
     }
+  }, 1500);
 }
-function updateRounds(){
-    ++roundCounter
-    rounds.innerText = roundCounter
+
+function track() {
+    if(lost){
+        return init()
+    }
+  playerPattern.push(event.target.id);
+  judger();
+  if (playerPattern.length === currentPattern.length) {
+   newRound();
+  }
 }
+  
+  function updateRound() {
+    ++roundCounter;
+    rounds.innerText = roundCounter;
+  }
+
 
 function judger() {
-    for(let i = 0; i<playerPattern.length; i++){
-        if(currentPattern[i] !== playerPattern){
-            headerText.innerText ="You lose Press Start to play again"
-            init()
-        }
+  for (let i = 0; i < playerPattern.length; i++) {
+    if (currentPattern[i] !== playerPattern[i]) {
+        lost = true
+      headerText.innerText = "You lose Press Start to play again";
+      return init();
     }
+  }
 }
 // function judger(){
 //     if(currentPattern.length === playerPattern.length)
 //         for(let i =0; i<playerPattern.length; i++){
 //             if( playerPattern[i] !== currentPattern[i])
 //            headerText.innerText ="You lose Press Start to play again"
-           
+
 //             }
 //             playerPattern= []
 //             updateRounds()
 //             randomizer()
 //             blinker()
 //     }
-function startGame(){
-    headerText.innerText= "Follow The Colors"
-    updateRounds()
-    randomizer()
-    blinker()
+function startGame() {
+  headerText.innerText = "Follow The Colors";
+  updateRound();
+  randomizer();
+  blinker();
+  if(lost){
+      return init()
+  }
 }
-function  newRound(){
-    updateRounds();
-    randomizer()
-    blinker()
+function newRound() {
+  updateRound();
+  randomizer();
+  blinker();
+  playerPattern = []
 }
